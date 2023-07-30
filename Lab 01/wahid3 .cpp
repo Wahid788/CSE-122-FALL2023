57fbff1d6f78304872729b71ad3acd40a4ae415a
Md.Abdul Wahid <140314180+Wahid788@users.noreply.github.com>
2023-07-30 16:28:20 +0600

    Add files via upload

---
+++ Lab 01/wahid3 .cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x>=0)
+    {
+        cout<<"x valu is positive"<<endl;
+        if(x%2==0)
+
+         cout<<"x valu is even"<<endl;
+        if(x>999 & x<10000)
+        {
+            cout<<"x valu contains 4 digits"<<endl;
+        }
+    }
+}

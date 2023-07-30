69cd0f219b31e49c603d272ec7e18f26a9d38070
Md.Abdul Wahid <140314180+Wahid788@users.noreply.github.com>
2023-07-30 15:05:33 +0600

    Add files via upload

---
+++ Lab 01/wahid .cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout <<"Hello in my university";
+    cout <<endl;
+    cout <<"\nIm a student in it faculty";
+    cout <<endl;
+    cout <<"nWelcome\n";
+}
